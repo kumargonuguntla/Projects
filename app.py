cfaff70e39851ec45dbd5ae13b8c7914070cdb36
kumargonuguntla <159897872+kumargonuguntla@users.noreply.github.com>
2024-12-05 19:40:47 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+import joblib as joblib
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
 # Load the trained model
